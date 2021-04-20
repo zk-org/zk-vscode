ec3dbce5961ae0a25140e88528d0635b0f575f22
Mickaël Menu <mickael.menu@gmail.com>
2021-04-20 20:56:06 +0200

    Minimal LSP client

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { workspace, ExtensionContext } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions
+} from 'vscode-languageclient/node';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zk-vscode" is now active!');
+let client: LanguageClient;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('zk-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+export function activate(context: ExtensionContext) {
+	const clientName = "zk"
+	const clientId = "zk"
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from zk-vscode!');
-	});
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run: { command: "zk", args: ["lsp"] },
+		debug: { command: "zk", args: ["lsp", "--log", "/tmp/zk-lsp.log"] }
+	};
 
-	context.subscriptions.push(disposable);
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for Markdown documents.
+		documentSelector: [{ scheme: 'file', language: 'markdown' }],
+	};
+
+	client = new LanguageClient(clientId, clientName, serverOptions, clientOptions);
+
+
+	// Start the client. This will also launch the server.
+	client.start();
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
